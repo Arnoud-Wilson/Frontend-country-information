7056b8597856f05a48782f3cdf1da1666c0002ad
Arnoud-Wilson <arnoud608@gmail.com>
2023-02-24 20:49:03 +0100

    part one afgemaakt

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
-console.log('Hallo daar!');
+import axios from"axios";
+
+
+async function getAllCountries() {
+    try {
+        const result = await axios.get("https://restcountries.com/v2/all");
+        const countries = result.data;
+        console.log(countries)
+
+        countries.sort((a, b) =>  a.population - b.population);
+
+        const listElement = document.getElementById("country-list");
+        const allCountries = countries.map((country) => {
+            return `
+            <li>
+                <img src="${country.flag}" alt="country flag" id="flag">
+                <p id="country-name" class=${country.region}>${country.name}</p>
+                <p id="population">Has e population of ${country.population}</p>
+            </li>    
+            `;
+        });
+        listElement.innerHTML = allCountries.join("");
+
+    }catch (e) {
+        console.error(e);
+    }
+}
+
+getAllCountries();
+
+
+
+
